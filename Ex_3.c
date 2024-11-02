fe56d58b611ed332bf0ab0d76c16dde024df667e
AAR-05 <160051854+AAR-05@users.noreply.github.com>
2024-11-01 19:33:57 -0600

    Ex_3.c

--- Ex_3.c
+++ Ex_3.c
@@ -2,21 +2,25 @@
    Matriz 3x3 imprimir valores Ciclo Do - While
    18 de octubre de 2024 */
 
-#include <stdio.h>
-
-int main ()
+int main()
 {
-    int arr [3][3] = {{1, 2, 3},{4, 5, 6},{7, 8, 9}};
-    int i = 0,j = 0;
-  
+    int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int i = 0, j = 0;
+   
     printf("Imprimir matriz\n");
+    
     do
     {
-        printf("%d   ", arr[i][j]);
-        j++; 
+        do
+        {
+            printf("%d | ", arr[i][j]);
+            j++;
+        }
+        while(j < 3);
+        printf("\n");
         j = 0;
         i++;
-        printf("\n"); 
-    }   while (i < 3 && j < 3);
+    }
+    while(i < 3);
     return 0;
 }
